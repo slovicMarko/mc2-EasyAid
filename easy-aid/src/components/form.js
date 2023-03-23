080100d12c76d600451ab7bc7d531546fcd612da
slovicMarko <marko.slovic03@gmail.com>
2023-03-23 20:46:16 +0100

    dodavanje login i sign up forma.js

---
+++ easy-aid/src/components/form.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+const formEvent = () => {
+  return (
+    <form class="form">
+      <div class="formField">
+        <input
+          class="input input--text"
+          id="formInput#text"
+          type="text"
+          name="text"
+          placeholder="Enter text"
+        />
+      </div>
+    </form>
+  );
+};
+
+export default formEvent;
