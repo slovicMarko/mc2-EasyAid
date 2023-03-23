4f151c74f6b942b9e2bc0a40548d18816b986bd6
zn1k4l3o <leo.znika@tvz.hr>
2023-03-23 23:34:37 +0100

    main i css

--- easy-aid/src/components/homeScreen.js
+++ easy-aid/src/components/homeScreen.js
@@ -1,12 +1,46 @@
 import React from 'react';
+//import sponzor1 from "../images";
   
-function HomeScreen (){
- return <address>
-            You can find us here:<br />
-            GeeksforGeeks<br />
-            5th & 6th Floor, Royal Kapsons, A- 118, <br />
-            Sector- 136, Noida, Uttar Pradesh (201305)
-        </address>
+function EmptySpace ({windowHeightPercentage}) {
+    return (
+        <div style={{ height: `${windowHeightPercentage}vh` }}> </div>
+    );
 }
+
+function HomeScreen () {
+    return(
+        <>
+            <EmptySpace windowHeightPercentage={15} />
+            <div className="front-windows">
+                <p className="front-window-font">
+                    Oduvijek ste željeli volontirati, a neznate kako početi?
+                </p>
+            </div>
+            <EmptySpace windowHeightPercentage={10} />
+
+            <div className="front-windows">
+                <p className="front-window-font">
+                    Bavite se organizacijom volonterskih akcija i planiranje Vam je oduvijek bio problem?
+                </p>
+            </div>
+            <EmptySpace windowHeightPercentage={10} />
+
+            <div className="front-windows">
+                <p className="front-window-font">
+                    ---
+                </p>
+            </div>
+            <EmptySpace windowHeightPercentage={5} />
+
+            <hr/>
+            <h2>Sponzori</h2>
+            <img className='sponsor-logo' src="../images/sponzor1.jpg" alt="Sponzor1"></img>
+            <img className='sponsor-logo' src="../images/sponzor2.jpg" alt="Sponzor2"></img>
+            <img className='sponsor-logo' src="../images/sponzor3.jpg" alt="Sponzor3"></img>
+            <hr/>
+
+        </>
+    );
+};
   
 export default HomeScreen;
