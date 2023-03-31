c2b0f370a83660d7a685a2cdd2ba8f5c126b17d2
slovicMarko <marko.slovic03@gmail.com>
2023-03-31 19:25:19 +0200

    malo CSSa i isprobavanje tipkovnicew

--- easy-aid/src/components/mainFeed.js
+++ easy-aid/src/components/mainFeed.js
@@ -1,6 +1,6 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import EventBubble from "./event.js";
-import {EmptySpace} from "./standard.js";
+import { EmptySpace } from "./standard.js";
 import ReactModal from "react-modal";
 
 function MainFeed() {
@@ -11,11 +11,11 @@ function MainFeed() {
     <div>
       <h1>Main Feed</h1>
       <div className="EventBubble">{EventBubble()}</div>
-      <EmptySpace windowHeightPercentage={5}/>
+      <EmptySpace windowHeightPercentage={5} />
       <div className="EventBubble">{EventBubble()}</div>
-      <EmptySpace windowHeightPercentage={5}/>
+      <EmptySpace windowHeightPercentage={5} />
       <div className="EventBubble">{EventBubble()}</div>
-      <EmptySpace windowHeightPercentage={5}/>
+      <EmptySpace windowHeightPercentage={5} />
       <div className="EventBubble">{EventBubble()}</div>
     </div>
   );
