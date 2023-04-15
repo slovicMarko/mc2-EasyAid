1eae58ef476ac792d2e506e041c79ee08e39a9ec
zn1k4l3o <leo.znika@tvz.hr>
2023-04-16 00:16:14 +0200

    napravljen osnovni layout Aktivne akcije

--- src/app/(routes)/activeEvents/page.js
+++ src/app/(routes)/activeEvents/page.js
@@ -1,8 +1,6 @@
 "use client";
 "use router";
 import React, { useState } from "react";
-import Head from "next/head";
-import Link from "next/link";
 import { useRouter } from "next/navigation";
 
 import { EventBubble } from "@/components/event";
@@ -25,18 +23,34 @@ function ActiveFeed() {
   return (
     <div>
       {finishedLoadingAndCanShow && (
-        <section id="sectionEvents">
-          <div className="activeEvents">
-            <h2>Aktivne akcije</h2>
-            <EventBubble isInActive/>
-            <EventBubble isInActive/>
-          </div>
-          <div className="pastEvents">
-            <h2>Prošle akcije</h2>
-            <EventBubble isInActive/>
-            <EventBubble isInActive/>
-          </div>
-        </section>
+        <div className="activeFeed">
+          <section id="section-events">
+            <div className="active-events">
+              <h2>Aktivne akcije</h2>
+              <EventBubble isInActive/>
+              <EventBubble isInActive/>
+            </div>
+            <div className="past-events">
+              <h2>Prošle akcije</h2>
+              <EventBubble isInActive/>
+              <EventBubble isInActive/>
+            </div>
+          </section>
+          <section id="section-info">
+            <div id="about-event">
+
+            </div>
+            <hr/>
+            <div className="overall-statistics">
+              <h2>Odrađenih akcija</h2>
+              <p>5</p>
+              <h2>Ukupno vrijeme rada</h2>
+              <p>8 sati</p>
+              <h2>Broj različitih organizacija</h2>
+              <p>3</p>
+            </div>
+          </section>
+        </div>        
       )}
     </div>
   );
