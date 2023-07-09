76f963b325e6f6e6ce374045319f7c940cf30a8d
slovicMarko <marko.slovic03@gmail.com>
2023-07-09 13:50:56 +0200

    vidljivo tko je sve prijavljen na akciju

--- src/app/(routes)/aktivne_akcije/[akcija]/page.js
+++ src/app/(routes)/aktivne_akcije/[akcija]/page.js
@@ -3,10 +3,13 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faPhone, faEnvelope } from "@fortawesome/free-solid-svg-icons";
 
-import { usePathname } from "next/navigation";
 import { useEffect, useState } from "react";
+import { usePathname } from "next/navigation";
 import Link from "next/link";
+import Image from "next/image";
+
 import { fetchEvents } from "@/firebase/fetchEvents";
+import { fetchUser } from "@/firebase/fetchUser";
 import {
   AddUserToEvent,
   RemoveUserFromEvent,
@@ -17,9 +20,9 @@ import { getAuth } from "firebase/auth";
 
 function Akcija() {
   const [action, setAction] = useState();
+  const [userList, setUserList] = useState();
   const [loading, setLoading] = useState(true);
   const pathname = usePathname().replace("/aktivne_akcije/", "");
-  const hasMaterials = false;
   const auth = getAuth();
 
   useEffect(() => {
@@ -32,6 +35,16 @@ function Akcija() {
         })
       )[0];
       setAction(response);
+      const users = await Promise.all(
+        response[0].registered.map((userID) =>
+          fetchUser({
+            collection: "users",
+            docField: "userID",
+            docValue: userID,
+          })
+        )
+      );
+      setUserList(users);
       setLoading(false);
     };
     fetchData();
@@ -101,22 +114,7 @@ function Akcija() {
               alt="slika akcije"
             />
           </div>
-          {/* <div>
-            {hasMaterials ? (
-              <div className="needed-stuff">
-                <h3>Potrebne stvari</h3>
-                <ul>
-                  <li>Dobra volja</li>
-                  <li>Osmijeh</li>
-                  <li>Humor</li>
-                </ul>
-              </div>
-            ) : (
-              <></>
-            )}
-          </div> */}
         </article>
-
         <article className="contacts">
           <iframe
             src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2780.846529252445!2d15.934562076801248!3d45.814331910008384!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4765d6d3ecf8bf1d%3A0xdda2ef01f37a0510!2sCrveni%20kri%C5%BE%20Zagreb!5e0!3m2!1shr!2shr!4v1685997560928!5m2!1shr!2shr"
@@ -135,6 +133,29 @@ function Akcija() {
             </p>
           </section>
         </article>
+        {action[1].userID === localStorage.getItem("user") ? (
+          <article className="registered-list">
+            <h2 className="registered-title">Prijavljeni volonteri</h2>
+            {userList.length ? (
+              userList.map((user, index) => (
+                <div className="registered-user" key={index}>
+                  <p>{index + 1}.</p>
+                  <div className="regustered-user-picture-container">
+                    <img
+                      className="registered-user-picture"
+                      src={user[0].photo}
+                    />
+                  </div>
+                  <p>
+                    {user[0].fname} {user[0].lname}
+                  </p>
+                </div>
+              ))
+            ) : (
+              <p>Nema prijavljenih volontera</p>
+            )}
+          </article>
+        ) : null}
       </section>
     </div>
   );
--- src/firebase/fetchUser.js
+++ src/firebase/fetchUser.js
@@ -4,36 +4,29 @@ import {
   where,
   collection,
   getDocs,
+  getDoc,
 } from "firebase/firestore";
 import { initializeApp } from "firebase/app";
 import firebaseConfig from "@/firebase/FirebaseConfig";
-import { getStorage, ref } from "firebase/storage";
 
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
-const storeage = getStorage(app);
 
 export async function fetchUser(data) {
-  const imageRef = ref(
-    storeage,
-    `users/${data.docValue}/profile_image/profile_picture`
-  );
-
   const user = new Array();
   try {
     const condition = query(
       collection(db, data.collection),
       where(data.docField, "==", data.docValue)
     );
+
     const querySnapshot = await getDocs(condition);
     querySnapshot.forEach((doc) => {
       user.push(doc.data());
       localStorage.setItem("userDocID", doc.id);
     });
-
   } catch (error) {
     console.log("Error getting documents:", error);
   }
-
   return user;
 }
